e2fa78f0da6a43ea74b44ede26971516461a79a9
Alex-0293 <1928311@tuta.io>
2020-06-07 20:13:25 +0300

    Add set IP function.

--- AlexkWindowsGuestUtils.psm1
+++ AlexkWindowsGuestUtils.psm1
@@ -15,7 +15,7 @@ function Set-KeyboardConfig {
         $Res = Invoke-Command -VMName $Using:VMName -ScriptBlock $ScriptBlock -credential $Using:GuestCredentials 
         return $res       
     }
-    Add-ToLog -Message "Setting VM [$VMName] keyboard parameters." -logFilePath $ScriptLogFilePath -display -status "Info"
+    Add-ToLog -Message "Setting VM [$VMName] guest keyboard parameters." -logFilePath $ScriptLogFilePath -display -status "Info"
     $res = Invoke-PSScriptBlock -Computer $Computer -Credential $Credentials -ScriptBlock $ScriptBlock -TestComputer -ExportedParameters $PSBoundParameters  
     return $res     
 }
@@ -35,7 +35,7 @@ function Set-DriveLabel {
         $Res = Invoke-Command -VMName $Using:VMName -ScriptBlock $ScriptBlock -credential $Using:GuestCredentials 
         return $res       
     }
-    Add-ToLog -Message "Setting VM [$VMName] volume label changing." -logFilePath $ScriptLogFilePath -display -status "Info"
+    Add-ToLog -Message "Setting VM [$VMName] guest volume label changing." -logFilePath $ScriptLogFilePath -display -status "Info"
     $res = Invoke-PSScriptBlock -Computer $Computer -Credential $Credentials -ScriptBlock $ScriptBlock -TestComputer -ExportedParameters $PSBoundParameters  
     return $res     
 }
@@ -48,13 +48,18 @@ function Set-GuestBGInfo {
         [System.Management.Automation.PSCredential] $GuestCredentials,
         [Parameter(Mandatory = $true, Position = 2, HelpMessage = "BGInfo installer URL." )]
         [ValidateNotNull()]
-        [string]$BGInfoInstallerURL 
+        [string]$BGInfoInstallerURL,
+        [Parameter(Mandatory = $true, Position = 3, HelpMessage = "BGInfo installer setting URL." )]
+        [ValidateNotNull()]
+        [string]$BGInfoInstallerSettingsURL  
     )
     $ScriptBlock = {
         $ScriptBlock = {
             Param(
-                [string] $BGInfoInstallerURL
+                [string] $BGInfoInstallerURL,
+                [string] $BGInfoInstallerSettingsURL
             )
+            #https://docs.microsoft.com/en-us/sysinternals/downloads/bginfo
             $FilePath = "$Env:USERPROFILE\Downloads\$(Split-Path -Path $BGInfoInstallerURL -Leaf)"
             Remove-Item $FilePath -Force -ErrorAction SilentlyContinue
             Invoke-WebRequest -Uri $BGInfoInstallerURL  -OutFile $FilePath
@@ -63,14 +68,22 @@ function Set-GuestBGInfo {
             Expand-Archive $FilePath -DestinationPath $BGInfoFolder
             Remove-Item  $FilePath -Force
 
-            Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Alex-0293/VMScripts/master/DATA/custom.bgi"  -OutFile "$BGInfoFolder\custom.bgi"
+            Invoke-WebRequest -Uri $BGInfoInstallerSettingsURL -OutFile "$BGInfoFolder\custom.bgi"
      
             & "$BGInfoFolder\bginfo64.exe" "$BGInfoFolder\custom.bgi" /accepteula /timer:0 /log "$BGInfoFolder\errors.log"  
             $desktopImage = "C:\Windows\BGInfo.bmp"
             
             Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name TileWallpaper  -value "0"           -Force
-            Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name WallpaperStyle -value "10"           -Force
+            Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name WallpaperStyle -value "10"          -Force
             Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name Wallpaper      -value $desktopImage -Force
+
+            $WshShell            = New-Object -comObject WScript.Shell
+            
+            $Shortcut                  = $WshShell.CreateShortcut("$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\StartUp\BGInfo.lnk")
+            $Shortcut.TargetPath       = "$BGInfoFolder\bginfo64.exe"
+            $Shortcut.Arguments        = "/iq `"custom.bgi`" /accepteula /timer:0"            
+            $Shortcut.WorkingDirectory = $BGInfoFolder
+            $Shortcut.Save()
             <#
                 Two registry values are set in the Control Panel\Desktop key.
                 
@@ -88,10 +101,10 @@ function Set-GuestBGInfo {
             #>
 
         }
-        Invoke-Command -VMName $Using:VMName -ScriptBlock $ScriptBlock -credential $Using:GuestCredentials -ArgumentList $Using:BGInfoInstallerURL  
+        Invoke-Command -VMName $Using:VMName -ScriptBlock $ScriptBlock -credential $Using:GuestCredentials -ArgumentList $Using:BGInfoInstallerURL, $Using:BGInfoInstallerSettingsURL  
     }
     
-    Add-ToLog -Message "Setting VM [$VMName] wallpaper." -logFilePath $ScriptLogFilePath -display -status "Info"
+    Add-ToLog -Message "Setting VM [$VMName] guest wallpaper." -logFilePath $ScriptLogFilePath -display -status "Info"
     $res = Invoke-PSScriptBlock -Computer $Computer -Credential $Credentials -ScriptBlock $ScriptBlock -TestComputer -ExportedParameters $PSBoundParameters  
 }
 function Start-WindowsInitialSetup {
@@ -131,12 +144,13 @@ function Start-WindowsInitialSetup {
 
             Set-Location -path $WinSettings
             & powershell.exe -NoProfile -ExecutionPolicy Bypass -File Win10.ps1 -include Win10.psm1 -preset $Preset -log output.log
+            Remove-Item -path "$Env:USERPROFILE\Desktop\*.*" -Force -ErrorAction SilentlyContinue 
         }
         
         Invoke-Command -VMName $Using:VMName -ScriptBlock $ScriptBlock -credential $Using:GuestCredentials -ArgumentList $Using:Preset 
     }
     
-    Add-ToLog -Message "VM [$VMName] windows initial setup." -logFilePath $ScriptLogFilePath -display -status "Info"
+    Add-ToLog -Message "Setting VM [$VMName] guest windows initial parameters with preset [$Preset]." -logFilePath $ScriptLogFilePath -display -status "Info"
     $res = Invoke-PSScriptBlock -Computer $Computer -Credential $Credentials -ScriptBlock $ScriptBlock -TestComputer -ExportedParameters $PSBoundParameters  
 }
 function Get-GuestWindowsOSVersion {
@@ -155,7 +169,7 @@ function Get-GuestWindowsOSVersion {
         $Res = Invoke-Command -VMName $Using:VMName -ScriptBlock $ScriptBlock -credential $Using:GuestCredentials 
         return $res       
     }
-    Add-ToLog -Message "Getting VM [$VMName] windows OS version." -logFilePath $ScriptLogFilePath -display -status "Info"
+    Add-ToLog -Message "Getting VM [$VMName] guest windows OS version." -logFilePath $ScriptLogFilePath -display -status "Info"
     $res = Invoke-PSScriptBlock -Computer $Computer -Credential $Credentials -ScriptBlock $ScriptBlock -TestComputer -ExportedParameters $PSBoundParameters  
     return $res    
     
@@ -189,7 +203,7 @@ function Rename-WindowsGuest {
         }
         Invoke-Command -VMName $Using:VMName -ScriptBlock $ScriptBlock -credential $Using:GuestCredentials -ArgumentList $Using:NewName, $Using:Description, $Using:GuestCredentials
     }    
-    Add-ToLog -Message "Renaming windows VM [$VMName] OS to [$NewName]." -logFilePath $ScriptLogFilePath -display -status "Info"
+    Add-ToLog -Message "Renaming VM [$VMName] windows guest OS to [$NewName]." -logFilePath $ScriptLogFilePath -display -status "Info"
     Invoke-PSScriptBlock -Computer $Computer -Credential $Credentials -ScriptBlock $ScriptBlock -TestComputer -ExportedParameters $PSBoundParameters   
 }
 function Enable-RDPAccess {
@@ -234,9 +248,158 @@ function Enable-RDPAccess {
         return $res     
     }    
 
-    Add-ToLog -Message "Enabling RDP access on VM [$VMName]." -logFilePath $ScriptLogFilePath -display -status "Info"
+    Add-ToLog -Message "Enabling RDP access on VM [$VMName] windows guest." -logFilePath $ScriptLogFilePath -display -status "Info"
     $res = Invoke-PSScriptBlock -Computer $Computer -Credential $Credentials -ScriptBlock $ScriptBlock -TestComputer -ExportedParameters $PSBoundParameters  
 
 }
+function Set-WinTaskBand {
+    param(
+        [Parameter(Mandatory = $true, Position = 0, HelpMessage = "VM name." )]
+        [ValidateNotNull()]
+        [string] $VMName,
+        [Parameter( Mandatory = $True, Position = 1, HelpMessage = "VM Credentials." )]
+        [System.Management.Automation.PSCredential] $GuestCredentials,
+        [Parameter(Mandatory  = $true, Position = 2, HelpMessage = "Taskband URL." )]
+        [ValidateNotNull()]
+        [string] $TaskbandURL
+    ) 
+    $ScriptBlock = {
+        $ScriptBlock = {
+            param(
+                [string] $TaskbandURL
+            )
+            $Folder = "$Env:USERPROFILE\Downloads\TaskBand"
+            Remove-Item -path $Folder -Recurse -force -ErrorAction SilentlyContinue
+            New-Item -Path $Folder -ItemType Directory -Force | Out-Null
+            $FilePath = "$Folder\taskband.zip"
 
-Export-ModuleMember -Function Set-KeyboardConfig, Set-DriveLabel, Set-GuestBGInfo, Start-WindowsInitialSetup, Get-GuestCredentials, Get-GuestWindowsOSVersion, Rename-WindowsGuest, Enable-RDPAccess
+            Invoke-WebRequest -Uri $TaskbandURL  -OutFile $FilePath
+            Expand-Archive $FilePath -DestinationPath $Folder
+            Remove-Item -path $FilePath -force
+  
+            Start-Process $env:windir\System32\reg.exe -ArgumentList "import $Folder\TaskbandCU.reg" -WindowStyle Hidden
+            Copy-Item -Path "$Folder\Quick launch\*" -Destination $env:UserProfile'\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch' -Recurse -Force -ErrorAction SilentlyContinue
+            Stop-Process -ProcessName explorer -Force -ErrorAction SilentlyContinue
+        }
+        Invoke-Command -VMName $Using:VMName  -credential $Using:GuestCredentials -ScriptBlock $ScriptBlock -ArgumentList $Using:TaskbandURL    
+    }
+    Add-ToLog -Message "Setting VM [$VMName] guest windows task band." -logFilePath $ScriptLogFilePath -display -status "Info"
+    Invoke-PSScriptBlock -Computer $Computer -Credential $Credentials -ScriptBlock $ScriptBlock -TestComputer -ExportedParameters $PSBoundParameters  
+}
+
+function Install-GuestWindowsFeature {
+    <#
+    .SYNOPSIS 
+        .AUTHOR Alexk
+        .DATE 07.05.2020
+        .VER 1   
+    .DESCRIPTION
+     Function to import or create new VM with custom parameters.
+    .EXAMPLE
+    Create new VM:
+        Add-NewCustomVM  -Computer "Host1" -Credentials $Credentials -NewVMName "Test" -Mode "New" -VMConfig $VMConfig -NETConfig $NETConfig -StartupConfig $StartupConfig -ISOFile $ISOFile -ImportPath $ImportPath -RDPShortcutsFolderPath $RDPShortcutsFolderPath -StartVM -AddIndex -StartRDPConsole
+
+    Import exported VM:
+        Add-NewCustomVM  -Computer "Host1" -Credentials $Credentials -NewVMName "Test" -Mode "Import" -ImportPath $ImportPath -VMTemplatePath $VMTemplatePath -RDPShortcutsFolderPath $RDPShortcutsFolderPath -StartVM
+    #> 
+    param(
+        [Parameter(Mandatory = $true, Position = 0, HelpMessage = "VM name." )]
+        [ValidateNotNull()]
+        [string] $VMName,
+        [Parameter( Mandatory = $True, Position = 1, HelpMessage = "VM Credentials." )]
+        [System.Management.Automation.PSCredential] $GuestCredentials,
+        [Parameter(Mandatory = $true, Position = 2, HelpMessage = "Argument list." )]
+        [ValidateNotNull()]
+        [array] $CommandList
+    ) 
+    $ScriptBlock = {
+        $ScriptBlock = {
+            param(
+                [array] $CommandList
+            )
+           
+
+            Invoke-WebRequest -Uri $TaskbandURL  -OutFile $FilePath
+            Expand-Archive $FilePath -DestinationPath $Folder
+            Remove-Item -path $FilePath -force  
+            
+        }
+        Invoke-Command -VMName $Using:VMName  -credential $Using:GuestCredentials -ScriptBlock $ScriptBlock -ArgumentList $Using:CommandList    
+    }
+    Add-ToLog -Message "Setting VM [$VMName] guest windows features" -logFilePath $ScriptLogFilePath -display -status "Info"
+    Invoke-PSScriptBlock -Computer $Computer -Credential $Credentials -ScriptBlock $ScriptBlock -TestComputer -ExportedParameters $PSBoundParameters 
+}
+
+function Set-GuestWindowsNetworkParameters {
+    <#
+    .SYNOPSIS 
+        .AUTHOR Alexk
+        .DATE 27.05.2020
+        .VER 1   
+    .DESCRIPTION
+     Function to set ip parameters in the VM guest windows OS.
+    .EXAMPLE
+   
+        Set-GuestWindowsNetworkParameters -VMName $VMName -GuestCredentials $GuestCredentials -IPAddress $IPAddress -NetMask $NetMask  -DefaultGateway $DefaultGateway -DNSServers $DNSServers -NICNumber $NICNumber
+    #>  
+    [CmdletBinding()]   
+    param(
+        [Parameter(Mandatory = $true,  Position = 0, HelpMessage = "VM name." )]
+        [ValidateNotNull()]
+        [string] $VMName,
+        [Parameter( Mandatory = $True, Position = 1, HelpMessage = "VM Credentials." )]
+        [System.Management.Automation.PSCredential] $GuestCredentials,
+        [Parameter(Mandatory = $true,  Position = 2, HelpMessage = "IP address." )]
+        [ValidateNotNull()]
+        [string] $IPAddress,
+        [Parameter(Mandatory = $true,  Position = 3, HelpMessage = "Network mask." )]
+        [ValidateNotNull()]
+        [string] $NetMask,
+        [Parameter(Mandatory = $true,  Position = 4, HelpMessage = "Default gateway." )]
+        [ValidateNotNull()]
+        [string] $DefaultGateway,
+        [Parameter(Mandatory = $false, Position = 5, HelpMessage = "DNS servers list." )]
+        [ValidateNotNull()]
+        [string] $DNSServers,
+        [Parameter(Mandatory = $true,  Position = 6, HelpMessage = "NIC number." )]
+        [ValidateNotNull()]
+        [int] $NICNumber
+    ) 
+    $ScriptBlock = {
+        $ScriptBlock = {
+            param(
+                [Parameter(Mandatory = $true, Position = 1, HelpMessage = "IP address." )]
+                [ValidateNotNull()]
+                [string] $IPAddress,
+                [Parameter(Mandatory = $true, Position = 2, HelpMessage = "Network mask." )]
+                [ValidateNotNull()]
+                [string] $NetMask,
+                [Parameter(Mandatory = $true, Position = 3, HelpMessage = "Default gateway." )]
+                [ValidateNotNull()]
+                [string] $DefaultGateway,
+                [Parameter(Mandatory = $false, Position = 4, HelpMessage = "DNS servers list." )]
+                [ValidateNotNull()]
+                [string] $DNSServers,
+                [Parameter(Mandatory = $true, Position = 5, HelpMessage = "NIC number." )]
+                [ValidateNotNull()]
+                [int] $NICNumber
+            ) 
+            
+            $NICs = Get-WMIObject Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -eq $true } | Select-Object -first $NICNumber
+            $NIC  = $NICs | Select-Object -Last 1
+            Write-Host "Selected Network adapter: $($NIC.Name)"
+
+            $NIC.EnableStatic($IPAddress , @($NetMask)) | out-null
+            $NIC.SetGateways($DefaultGateway) | Out-Null
+            if ($DNSServers) {
+                $NIC.SetDNSServerSearchOrder($DNSServers) | Out-Null
+            }
+            $NIC.SetDynamicDNSRegistration("False") | Out-Null
+        }
+        Invoke-Command -VMName $Using:VMName  -credential $Using:GuestCredentials -ScriptBlock $ScriptBlock -ArgumentList $Using:IPAddress, $Using:NetMask, $Using:DefaultGateway, $Using:DNSServers, $Using:NICNumber    
+    }
+    Add-ToLog -Message "Setting VM [$VMName] guest windows ip parameters." -logFilePath $ScriptLogFilePath -display -status "Info"
+    Invoke-PSScriptBlock -Computer $Computer -Credential $Credentials -ScriptBlock $ScriptBlock -TestComputer -ExportedParameters $PSBoundParameters 
+}
+
+Export-ModuleMember -Function Set-KeyboardConfig, Set-DriveLabel, Set-GuestBGInfo, Start-WindowsInitialSetup, Get-GuestCredentials, Get-GuestWindowsOSVersion, Rename-WindowsGuest, Enable-RDPAccess, Set-WinTaskBand, Install-GuestWindowsFeature, Set-GuestWindowsNetworkParameters
